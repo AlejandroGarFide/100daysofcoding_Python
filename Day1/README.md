f3272137275d6ab847b7a5481837b32ee78551b1
Alejandro García <vi01079@innovaccion.mx>
2021-11-12 13:45:41 -0600

    Adding the 1st project of the challenge.

---
+++ Day1/README.md
@@ -0,0 +1,3 @@
+# Proyecto número 1 del #100DaysofCoding Python
+
+En este proyecto, se exploran las funciones de "print" y de "input" y así crear un nombre para una banda de jazz.
---
+++ Day1/band_generator.py
@@ -0,0 +1,10 @@
+#1. Create a greeting for your program.
+print("¡Hola! Y bienvenido al generador de nombres de bandas de jazz.")
+#2. Ask the user for the city that they grew up in.
+ciudad = input("Introduce el nombre de tu ciudad o municipio:\n")
+#3. Ask the user for the name of a pet.
+mascota = input("Ahora dime el nombre de tu mascota:\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("El nombre de tu banda podría ser " + ciudad + " " + mascota)
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://band-name-generator-end.appbrewery.repl.run/
